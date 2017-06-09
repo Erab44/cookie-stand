37b345415c81b9150c77242e8f0ef53b679d6222
Jeff McCord <jeff.mccord44@gmail.com>
2017-06-09 14:32:26 -0700

    added min, max, and average cookie

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 'use strict'
 
 var firstAndPike = {
-
+  minCust: 23,
+  maxCust: 65,
+  avgCookiePerSale: 6.3
 };
 
 var seaTacAir = {
-
+  minCust: 3,
+  maxCust: 24,
+  avgCookiePerSale: 1.2
 };
 
 var seattleCenter = {
-
+  minCust: 11,
+  maxCust: 38,
+  avgCookiePerSale: 3.7
 };
 
 var capHill = {
-
+  minCust: 20,
+  maxCust: 38,
+  avgCookiePerSale: 2.3
 };
 
 var alki = {
-
+  minCust: 2,
+  maxCust: 16,
+  avgCookiePerSale: 4.6
 };
