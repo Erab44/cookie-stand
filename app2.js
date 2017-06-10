03b0b5e3acbf5f2db02d38a9f02ec4cfa9b9a40f
Jeff McCord <jeff.mccord44@gmail.com>
2017-06-09 20:43:54 -0700

    added new js file to play with, erased nonsense from html

---
+++ app2.js
@@ -0,0 +1,53 @@
+'use strict'
+
+var firstAndPike = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookiePerSale: 6.3
+};
+
+var seaTacAir = {
+  minCust: 3,
+  maxCust: 24,
+  avgCookiePerSale: 1.2
+};
+
+var seattleCenter = {
+  minCust: 11,
+  maxCust: 38,
+  avgCookiePerSale: 3.7
+};
+
+var capHill = {
+  minCust: 20,
+  maxCust: 38,
+  avgCookiePerSale: 2.3
+};
+
+var alki = {
+  minCust: 2,
+  maxCust: 16,
+  avgCookiePerSale: 4.6
+};
+
+var storesMinMax = [
+  {fpMin: 23, fpMax: 65},
+  {airMin: 3, airMax: 24},
+  {scMin: 11, scMax: 38},
+  {capMin: 20, capMax: 38},
+  {alkiMin: 2, alkiMax: 16}
+];
+
+var avgCookiePerSaleStore = [
+  {firstAndPike: 6.3},
+  {seaTacAir: 1.2},
+  {seattleCenter: 3.7},
+  {capHill: 2.3},
+  {alki: 4.6}
+];
+
+var randomCustPerHour = Math.random() * (maxCust - minCust) + minCust;
+
+function custHour(minCust, maxCust) {
+  return Math.random() * (maxCust - minCust) + minCust;
+}
