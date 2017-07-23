f5eca3525a5cdde1daf38fb3400560f0cdf04b0a
Jeff McCord <jeff.mccord44@gmail.com>
2017-07-22 18:34:47 -0700

    added a few new lines of code and cleaned it up

--- app.js
+++ app.js
@@ -1,44 +1,25 @@
 'use strict';
 
-// function cookiesPerHour() {
-//   for(var i = 0; i < store.hoursOpen.length; i++){
-//     store.averageCustHour = storeNumber(store.minCust, store.maxCust);
-//
-//   }
-//}
 function salesPerStore(store){
   var sectionEl = document.createElement('section');
-  //step 1
   var headerEl = document.createElement('h4');
   var listEl = document.createElement('ul');
   var listItemEl = document.createElement('li');
-  //step 2
   sectionEl.appendChild(headerEl);
   headerEl.appendChild(listEl);
   listEl.appendChild(listItemEl);
 }
 
-//tableEl.appendChild(tableBodyEl);
-// var tableEl = document.getElementById('generated-table');
-//
-// var tableBodyEl = document.createElement('tbody');
-// tableEl.appendChild(tableBodyEl);
-
-// var allStores = [firstAndPike, seaTacAir, seattleCenter, capHill, alki];
 var hoursOpen = ['6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM'];
 
 var salesWork = document.getElementById('sales-worksheet');
-// var totalCookies =
-// for (var i = 0; i < allStores.length; i++) {
-//   salesPerStore(allStores[i]);
-// }
+
 function Store(name, minCust, maxCust, avgCookiePerSale){
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookiePerSale = avgCookiePerSale;
   this.averageCustHour = 0;
-  // this.totalCookies = totalCookies;
   this.results = [];
   this.totalSales = 0;
 }
@@ -49,15 +30,6 @@ var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
 var capHill = new Store('Capital Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
-// var seaTacAir = {
-//   minCust: 3,
-//   maxCust: 24,
-//   avgCookiePerSale: 1.2,
-//   averageCustHour: 0,
-//   cookiesBoughtHour: [],
-//   totalCookies: 0,
-//   hoursOpen: ['6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM']
-// };
 Store.prototype.randnum = function() {
   return Math.floor(Math.random() * ((this.maxCust + 1) - this.minCust) + this.minCust);
 };
@@ -71,20 +43,11 @@ Store.prototype.makeRow = function(){
   nameDataEl.textContent = this.name;
   newRowEl.appendChild(nameDataEl);
 
-  // var minCustEl = document.createElement('td');
-  // // minCustEl.textContent = this.minCust;
-  // newRowEl.appendChild(minCustEl);
-  //
-  // var maxCustEl = document.createElement('td');
-  // // maxCustEl.textContent = this.maxCust;
-  // newRowEl.appendChild(maxCustEl);
-
-  for (var i=0; i < this.results.length; i++) {
+  for (var i = 0; i < this.results.length; i++) {
     var insertEl = document.createElement('td');
     insertEl.textContent = this.results[i];
     newRowEl.appendChild(insertEl);
   }
-
 
   var totalsEl = document.createElement('td');
   totalsEl.setAttribute('class', 'totals');
@@ -95,17 +58,11 @@ Store.prototype.makeRow = function(){
 
 };
 
-
-
-
-
-
-
 var allStores = [firstAndPike, seaTacAir, seattleCenter, capHill, alki];
 
 for (var s = 0; s < allStores.length; s++) {
   var currentStore = allStores[s];
-  for (var i = 1; i <=  hoursOpen.length; i++) {
+  for (var i = 1; i <= hoursOpen.length; i++) {
     currentStore.hourlySales = Math.floor((currentStore.randnum() * currentStore.avgCookiePerSale));
     currentStore.totalSales += currentStore.hourlySales;
     var currentHour = hoursOpen[i];
@@ -114,5 +71,22 @@ for (var s = 0; s < allStores.length; s++) {
   currentStore.makeRow();
 }
 
-// var newResultsArray = Store[this.results];
-// var newResults = this.results;
+var form = document.getElementById('form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(event.target.storeName.value);
+  console.log(event.target.minCust.value);
+  console.log(event.target.maxCust.value);
+  console.log(event.target.avgCookiePerSale.value);
+
+  var storeName = event.target.storeName.value;
+  var minCust = event.target.minCust.value;
+  var maxCust = event.target.maxCust.value;
+  var avgCust = event.target.avgCookiePerSale.value;
+
+  var newStore = new Store(storeName, minCust, maxCust, avgCookiePerSale);
+
+}
